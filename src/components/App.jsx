cece8387fd2853f47c5f809d2db5ea621b7585b4
PoLoMoTo <cbarnwel@stevens.edu>
2018-04-27 17:39:58 -0400

    Added card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import LoginForm from './LoginForm'
+import Card from './Card'
 import '../style/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginForm />
+        <Card>
+          <LoginForm />
+        </Card>
       </div>
     );
   }
