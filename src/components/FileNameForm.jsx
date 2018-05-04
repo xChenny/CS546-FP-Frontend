0844fae08af6aa3909440e5892b12dfd0888f65b
PoLoMoTo <cbarnwel@stevens.edu>
2018-05-04 19:38:32 -0400

    Attempted to fix creating a new file

--- src/components/FileNameForm.jsx
+++ src/components/FileNameForm.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { Redirect } from "react-router"
+
+const createFile = e => {
+  const fn = e.target.fileName.value;
+  axios.post(`/s3/${fn}`);
+  return (<Redirect to={`/editor/${fn}`}/>)
+};
 
 const FileNameForm = () => {
   return (
-    <form action='/create/file' method='POST'>
+    <form onSubmit={e => createFile(e)}>
       <aside>File Name:</aside>
-      <input type='text' name='fileName' />
+      <input type="text" name="fileName" />
       <br />
-      <button type='submit'>Submit</button>
+      <button type="submit">Submit</button>
       <br />
     </form>
-  )
-}
-export default FileNameForm
+  );
+};
+export default FileNameForm;
