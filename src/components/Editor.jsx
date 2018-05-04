d31643908eb09c217828e77e728d8709bfd5a927
PoLoMoTo <cbarnwel@stevens.edu>
2018-05-04 15:15:52 -0400

    Got editor pulling from S3

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,55 +1,61 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import {withRouter} from 'react-router-dom'
-import MonacoEditor from 'react-monaco-editor'
+import React, { Component } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import MonacoEditor from "react-monaco-editor";
 
 class Editor extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       code: "var thing = 'thing'"
-    }
+    };
   }
 
-  async editorDidMount (editor, monaco) {
-    // console.log('editorDidMount', editor)
-    const { id } = this.props.location
-    const codes = await axios.get(`/s3/${id}`)
-    console.log(codes)
-    editor.focus()
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const filePromise = await axios.get(`/s3/${id}`);
+    const file = filePromise.data;
+    console.log(file);
+    await this.setState({ file });
   }
 
-  onChange (newValue, e) {
-    console.log('onChange', newValue, e)
+  async editorDidMount(editor, monaco) {
+    editor.focus();
   }
 
-  render () {
-    const { code } = this.state
+  onChange(newValue, e) {
+    console.log("onChange", newValue, e);
+  }
+
+  render() {
+    const { code, file } = this.state;
     const config = {
       options: {
         selectOnLineNumbers: true
       },
       requireConfig: {
-        url: 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.1/require.min.js',
+        url:
+          "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.1/require.min.js",
         paths: {
-          vs: 'https://as.alipayobjects.com/g/cicada/monaco-editor-mirror/0.6.1/min/vs'
+          vs:
+            "https://as.alipayobjects.com/g/cicada/monaco-editor-mirror/0.6.1/min/vs"
         }
       }
-    }
+    };
     return (
       <div>
         <MonacoEditor
-          width='800'
-          height='600'
-          language='javascript'
-          value={code}
+          width="800"
+          height="600"
+          language="javascript"
+          value={file}
           options={config.options}
           requireConfig={config.requireConfig}
           editorDidMount={this.editorDidMount}
         />
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(Editor)
+export default withRouter(Editor);
